a89ec2ba5ae5254ad828b3b4d7233b17f72a917a
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2021-11-29 10:54:54 +0100

    Create indentHeading.js

---
+++ docs/javascripts/indentHeading.js
@@ -0,0 +1,22 @@
+var dict = {
+  "H2" : "5%",
+  "H3" : "10%",
+  "H4" : "15%",
+  "H5" : "20%",
+  "H6" : "25%",
+  "H7" : "30%"
+};
+
+
+$('.toto').children().each(function () {
+//alert($(this).prev().prop('nodeName'));
+		if ( ["H2","H3","H4","H5","H4"].includes($(this).prop('nodeName')) ){
+      if (typeof toggle !== 'undefined'){
+      	$(this).css("margin-left",dict[toggle]);
+      }
+      toggle=$(this).prop('nodeName')
+    }
+    else if (typeof toggle !== 'undefined'){
+    	$(this).css("margin-left",dict[toggle]);
+    }
+});
