a93560790ecf45a19fbbfffb7acb3b1ad79a045b
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2021-11-29 14:20:11 +0100

    Update indentHeading.js

--- docs/javascripts/indentHeading.js
+++ docs/javascripts/indentHeading.js
@@ -10,13 +10,37 @@ var dict = {
   "H7" : "30%"
 };
 
+var styles = `
+    h2 { 
+   	margin-left: 1%;
+    }
+    h3 { 
+   	margin-left: 5%;
+    }
+    h4 { 
+   	margin-left: 10%;
+    }
+    h5 { 
+   	margin-left: 15%;
+    }
+    h6 { 
+   	margin-left: 20%;
+    }
+    
+`
+var styleSheet = document.createElement("style")
+styleSheet.type = "text/css"
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
+
+
 
 $('article').children().each(function () {
 //alert($(this).prev().prop('nodeName'));
 	if ( ["H2","H3","H4","H5","H4"].includes($(this).prop('nodeName')) ){
-      		if (typeof toggle !== 'undefined'){
-      			$(this).css("margin-left",dict[toggle]);
-      		}
+      		//if (typeof toggle !== 'undefined'){
+      		//	$(this).css("margin-left",dict[toggle]);
+      		//}
       		toggle=$(this).prop('nodeName')
     	}
     	else if (typeof toggle !== 'undefined'){
